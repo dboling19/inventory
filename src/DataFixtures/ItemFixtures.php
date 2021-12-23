05c9a54772986727cb8609a5e08f7ff978462c47
Daniel Boling <dbdanielbboling70@gmail.com>
2021-12-22 23:55:53 -0500

    Update Commit

--- src/DataFixtures/ItemFixtures.php
+++ src/DataFixtures/ItemFixtures.php
@@ -4,12 +4,31 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Item;
 
 class ItemFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
 
+        $loc = new Location();
+        $loc->setName('Freezer');
+        $manager->persist($loc);
+
+        $loc = new Location();
+        $loc->setName('Cupboard');
+        $manager->persist($loc);
+
+        $item = new Item();
+        $item->setName('Ketchup');
+        $item->setQuantity(2);
+        $item->setLocation();
+
         $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 2;
     }
 }
